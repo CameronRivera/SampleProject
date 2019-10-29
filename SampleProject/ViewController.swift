67eaa3b46a3de056b6caf53e9a84745a74f749dc
CameronRivera <cameronrivera@pursuit.org>
2019-10-29 11:24:15 -0400

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Cameron Rivera on 10/29/19.
+//  Copyright © 2019 Cameron Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
